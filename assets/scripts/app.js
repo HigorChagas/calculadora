67000381b13bf1120203b2871bf1c8bdead4972b
Higor Chagas <higornchagas@gmail.com>
2022-10-24 13:42:34 -0300

    Terminando funcao de keycodes do numpap

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,35 +9,65 @@ function calculadora() {
 
     document.addEventListener('keydown', (e) => {
         switch(e.code) {
+            case 'Backspace':
+            case 'Delete':
+                apagarNumero();
+                break;
             case 'Numpad0':
+            case 'Digit0':
                 display.value += 0;
                 break;
             case 'Numpad1':
+            case 'Digit1':
                 display.value += 1;
                 break;
             case 'Numpad2':
+            case 'Digit2':
                 display.value += 2;
                 break;
             case 'Numpad3':
+            case 'Digit3':
                 display.value += 3;
                 break;
             case 'Numpad4':
+            case 'Digit4':
                 display.value += 4;
                 break;
             case 'Numpad5':
+            case 'Digit5':
                 display.value += 5;
                 break;
             case 'Numpad6':
+            case 'Digit6':
                 display.value += 6;
                 break;
             case 'Numpad7':
+            case 'Digit7':
                 display.value += 7;
                 break;
             case 'Numpad8':
+            case 'Digit8':
                 display.value += 8;
                 break;
             case 'Numpad9':
+            case 'Digit9':
                 display.value += 9;
+                break;
+            case 'NumpadAdd':
+                display.value += '+';
+                break;
+            case 'NumpadMultiply':
+                display.value += '*';
+                break
+            case 'NumpadSubtract': 
+                display.value += '-';
+                break;
+            case 'NumpadDivide':
+                display.value += '/';
+                break;
+            case 'NumpadComma':
+            case 'Period':
+                display.value += '.'
                 break;
         }
     });
