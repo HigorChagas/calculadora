9e1ebeb4137908cfad675d201179457ca315492c
HigorChagas <higornchagas@gmail.com>
2023-03-22 14:12:46 -0300

    Refatorando código

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,121 +1,71 @@
-function calculadora() {
-    
-    const display = document.querySelector('.calc__input');
+class Calculadora {
+    constructor() {
+        this.display = document.querySelector('.calc__input');
 
-    document.addEventListener('keydown', (e) => {
-        if(e.key === 'Enter') {
-            calcular()
-        }
-    });
+        document.addEventListener('click', (event) => {
+            const elemento = event.target;
 
-    document.addEventListener('keydown', (e) => {
-        switch(e.code) {
-            case 'Backspace':
-            case 'Delete':
-                apagarNumero();
-                break;
-            case 'Numpad0':
-            case 'Digit0':
-                display.ariaValueNow += 0;
-                break;
-            case 'Numpad1':
-            case 'Digit1':
-                display.value += 1;
-                break;
-            case 'Numpad2':
-            case 'Digit2':
-                display.value += 2;
-                break;
-            case 'Numpad3':
-            case 'Digit3':
-                display.value += 3;
-                break;
-            case 'Numpad4':
-            case 'Digit4':
-                display.value += 4;
-                break;
-            case 'Numpad5':
-            case 'Digit5':
-                display.value += 5;
-                break;
-            case 'Numpad6':
-            case 'Digit6':
-                display.value += 6;
-                break;
-            case 'Numpad7':
-            case 'Digit7':
-                display.value += 7;
-                break;
-            case 'Numpad8':
-            case 'Digit8':
-                display.value += 8;
-                break;
-            case 'Numpad9':
-            case 'Digit9':
-                display.value += 9;
-                break;
-            case 'NumpadAdd':
-                display.value += '+';
-                validarSinais(display);
-                break;
-            case 'NumpadMultiply':
-                display.value += '*';
-                break
-            case 'NumpadSubtract': 
-                display.value += '-';
-                validarSinais(display);
-                break;
-            case 'NumpadDivide':
-                display.value += '/';
-                break;
-            case 'NumpadComma':
-            case 'Period':
-                display.value += '.'
-                break;
-        }
-    });
+            if (elemento.classList.contains('calc__operations')) {
+                const ultimoCaractere = this.display.value.slice(-1);
+                if (/[\+\-\*/]/.test(ultimoCaractere)) {
+                    return;
+                }
+                return (this.display.value += elemento.value);
+            }
+            if (elemento.classList.contains('calc__clear')) {
+                this.limparDisplay();
+            }
+            if (elemento.classList.contains('calc__numbers')) {
+                return (this.display.value += elemento.value);
+            }
+            if (elemento.classList.contains('calc__del')) {
+                return this.apagaDigito();
+            }
+            if (elemento.classList.contains('calc__eq')) {
+                return this.calcular();
+            }
+        });
 
-    document.addEventListener('click', (e) => {
-        const elemento = e.target;
-        if(elemento.classList.contains('calc__clear')) {
-            limparDisplay();
-        }
-        if(elemento.classList.contains('calc__numbers')) {
-            display.value += elemento.value;
-            validarSinais(display)
-        }
-        if(elemento.classList.contains('calc__del')) {
-            apagarNumero();
-        }
-        if(elemento.classList.contains('calc__eq')) {
-            calcular();
-        }
-    });
-
-    const limparDisplay = () => {
-        display.value = '';
+        document.addEventListener('keydown', (e) => {
+            if (/Backspace|Delete/.test(e.code)) {
+                this.apagaDigito();
+            } else if (/Numpad[0-9]|Digit[0-9]/.test(e.code)) {
+                this.display.value += e.key;
+            } else if (/(NumpadAdd|NumpadSubtract)/.test(e.code)) {
+                this.display.value += e.key;
+            } else if (/NumpadMultiply|NumpadDivide/.test(e.code)) {
+                this.display.value += e.key;
+            } else if (/NumpadComma|Period/.test(e.code)) {
+                this.display.value += '.';
+            }
+        });
     }
 
-    const apagarNumero = () => {
-        display.value = display.value.slice(0, -1);
-    } 
+    limparDisplay() {
+        return (this.display.value = '');
+    }
 
-    const calcular = () => {
-        let conta = display.value;
-        
-        if(!conta) {
-            alert('Conta inválida!');
-            display.value = '';
+    apagaDigito() {
+        const ultimoCaractere = this.display.value.slice(-1);
+        if (/[+\-*/]/.test(ultimoCaractere)) {
+            return (this.display.value = this.display.value.slice(0, -2));
         } else {
-            display.value = eval(conta);
+            return (this.display.value = this.display.value.slice(0, -1));
         }
-    } 
+    }
 
-    const validarSinais = (sinal) => {
-        const operations = ['+', '-', '*', '/'];
-        
-        
+    calcular() {
+        let resultado = this.display.value;
+
+        try {
+            this.display.value = eval(resultado);
+        } catch (error) {
+            this.limparDisplay();
+            alert('Conta inválida');
+            throw new Error('Conta inválida');
+        }
     }
 }
 
-calculadora();
+const calculadora = new Calculadora();
+calculadora;
